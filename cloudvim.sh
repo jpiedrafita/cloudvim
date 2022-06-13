20d667644cf8ec697f97525ad5c42f56c7014c87
jpiedrafita <4170476+jpiedrafita@users.noreply.github.com>
2022-06-13 12:26:31 +0000

    cloudvim.sh

--- cloudvim.sh
+++ cloudvim.sh
@@ -3,4 +3,6 @@ sudo yum update -y
 sudo yum install -y git vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
-wget -O - $HOME/.vimrc htts://raw.githubusercontent.com/jpiedrafita/cloudvim/main/.vimrc
+wget -O - $HOME/.vimrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/.vimrc
+wget -O - $HOME/.bashrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/.bashrc
+wget -O - $HOME/aliases https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/aliases
