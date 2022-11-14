00c1502e89f8efd3fcb8f807699791f3691ac3ce
Jorge <4170476+jpiedrafita@users.noreply.github.com>
2022-11-14 14:59:57 +0100

    Update cloudvim.sh

--- cloudvim.sh
+++ cloudvim.sh
@@ -6,3 +6,5 @@ git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plug
 wget $HOME/.vimrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/.vimrc
 echo "source $HOME/aliases" >> $HOME/.bashrc
 wget $HOME/aliases https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/aliases
+export PS1="\n\e[1;32m[\u@\h \W]\$ \e[m "
+# export PS1='\n\[\033[01;32m\]\u@\H:\[\033[02;36m\] \w \$\[\033[00m\] '
