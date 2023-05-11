1a525eed850e4649339c0dd6d051406b2ac81b6f
Jorge <4170476+jpiedrafita@users.noreply.github.com>
2023-05-11 09:57:22 +0200

    Added yum or apt-get: fix

--- cloudvim.sh
+++ cloudvim.sh
@@ -7,7 +7,7 @@ if command -v apt-get &> /dev/null; then
 elif command -v yum &> /dev/null; then
     sudo yum update -y
     sudo yum install -y git vim
-
+fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
@@ -15,4 +15,3 @@ wget $HOME/.vimrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/
 echo "source $HOME/aliases" >> $HOME/.bashrc
 wget $HOME/aliases https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/aliases
 export PS1="\n\e[1;32m[\u@\h \W]\$ \e[m "
-# export PS1='\n\[\033[01;32m\]\u@\H:\[\033[02;36m\] \w \$\[\033[00m\] '
