e966946aeb0d8926c7367037dc9dcb2f06f6ed9e
Jorge <4170476+jpiedrafita@users.noreply.github.com>
2023-05-17 09:16:36 +0200

    Reworking script

--- README.md
+++ README.md
@@ -11,7 +11,24 @@ curl -sSL https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/cloudvim
 ```
 
 # Install plugins in vim
-:PluginInstall
+Run :PluginInstall in vim
+
+or
+
+```
+vim +PluginInstall +qall
+```
 
 # Load aliases
 source .bashrc
+
+# EC2 User data
+```
+# Descargar y ejecutar el script remoto
+wget -O /tmp/cloudvim.sh https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/cloudvim.sh
+chmod +x /tmp/cloudvim.sh
+/tmp/cloudvim.sh
+
+# Ejecutar :PluginInstall en Vim como el usuario ec2-user
+sudo -u ec2-user -H sh -c "vim +PluginInstall +qall"
+```
--- cloudvim.sh
+++ cloudvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 if command -v apt-get &> /dev/null; then
     echo "Using apt-get"
     sudo apt-get update -y
@@ -9,13 +11,15 @@ elif command -v yum &> /dev/null; then
     sudo yum install -y git vim
 fi
 
-#Vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
-wget $HOME/.vimrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/.vimrc
-vim +PluginInstall +qall
-
 #shell
 echo "source $HOME/aliases" >> $HOME/.bashrc
 wget $HOME/aliases https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/aliases
 export PS1="\n\e[1;32m[\u@\h \W]\$ \e[m "
+
+#Vim
+wget $HOME/.vimrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
+vim +PluginInstall +qall
+
+set +x
