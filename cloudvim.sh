8a8e056f27c43ca07ae13e51e0f19daece8e8ec7
Jorge <4170476+jpiedrafita@users.noreply.github.com>
2023-05-17 09:58:20 +0200

    Autoapprove en vim PluginInstall

--- cloudvim.sh
+++ cloudvim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 if command -v apt-get &> /dev/null; then
     echo "Using apt-get"
     sudo apt-get update -y
@@ -14,18 +12,14 @@ fi
 #shell
 #export PS1="\n\e[1;32m[\u@\h \W]\$ \e[m "
 echo 'export PS1="\n\e[1;32m[\u@\h \W]\$ \e[m "' >> ~/.bashrc
-
-
 echo "source $HOME/aliases" >> $HOME/.bashrc
- 
-wget $HOME/aliases https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/aliases
+ wget $HOME/aliases https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/aliases
 
 #Vim
 wget $HOME/.vimrc https://raw.githubusercontent.com/jpiedrafita/cloudvim/master/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
-vim +PluginInstall +qall
+vim +PluginInstall +qall -v 'qa!'
 
 source $HOME/.bashrc
 
-set +x
